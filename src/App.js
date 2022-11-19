14334ba0bc720ef7278b65cd134c2b9e361239d5
DARKSTYLE <117010263+ilyeschrif@users.noreply.github.com>
2022-11-20 00:00:33 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+
+import Header from './common/header/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <>
+     <Header />
+      </>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/common/header/Head.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Head = () => {
+  return (
+    <>
+    
+   <section className='head'>
+   <div className='container'>
+   <div className='left row'>
+   <i className='fa fa-phone'></i>
+   <label>+21623811673</label>
+   <i className='fa fa-envelope'></i>
+   <label>lyeschrif50@gmail.com</label>
+   </div>
+   </div>
+   </section>
+
+    </>
+  )
+}
+
+export default Head
